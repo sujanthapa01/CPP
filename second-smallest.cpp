4037d103e2b839c7f198b4220ef889f334055cb5
sujanthapa01 <sujanthapa4001@gmail.com>
2024-10-13 11:32:12 +0530

    comiit

---
+++ second-smallest.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int nums[] = {7, 3, 5, 2, 8, 7, 5};
+    int size = sizeof(nums) / sizeof(nums[0]);
+
+    for (int i = 0; i < size - 1; i++)
+    {
+        bool swapped = false;
+        for (int j = 0; j < size - i - 1; j++)
+        {
+            if (nums[j] > nums[j + 1])
+            {
+                int temp = nums[j];
+                nums[j] = nums[j + 1];
+                nums[j + 1] = temp;
+                swapped = true;
+            }
+        }
+
+        // Check if no elements were swapped in this pass, then the array is sorted
+        if (!swapped)
+        {
+            break;
+        }
+    }
+
+    // Print the sorted array
+    for (int i = 0; i < size; i++)
+    {
+        cout << nums[i] << " ";
+    }
+    cout << endl;
+
+    // Print the second largest number
+    cout << nums[1] << " is the second largest number" << endl;
+
+    return 0;
+}
